ad030b8ebeb452b962f05da356bed48a232ca4ef
TheFuzz_25 <104623695+thefuzz25@users.noreply.github.com>
2025-08-10 13:42:17 +0530

    Delete _plugins/parse_bit_datetime.rb

--- _plugins/parse_bit_datetime.rb
+++
@@ -1,7 +0,0 @@
-Jekyll::Hooks.register :documents, :pre_render do |doc|
-  if doc.collection.label == "bits"
-    # Use current local time
-    doc.data['date'] = Time.now
-    doc.data['title'] = nil # Prevent accidental title usage
-  end
-end
