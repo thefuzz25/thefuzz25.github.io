94c3be7d0cb7dfb1fa68684fe19cf6f0720f6895
TheFuzz_25 <104623695+thefuzz25@users.noreply.github.com>
2025-08-10 13:18:21 +0530

    Update parse_bit_datetime.rb

--- _plugins/parse_bit_datetime.rb
+++ _plugins/parse_bit_datetime.rb
@@ -1,13 +1,7 @@
-# _plugins/bit_filename_parser.rb
-
 Jekyll::Hooks.register :documents, :pre_render do |doc|
   if doc.collection.label == "bits"
-    if doc.basename_without_ext =~ /^(\d{4})-(\d{2})-(\d{2})-(\d{2})(\d{2})$/
-      year, month, day, hour, minute = $1, $2, $3, $4, $5
-      t = Time.new(year.to_i, month.to_i, day.to_i, hour.to_i, minute.to_i)
-      doc.data['date'] = t
-      doc.date = t  # This ensures Liquid uses the correct time
-      doc.data['title'] = nil
-    end
+    # Use current local time
+    doc.data['date'] = Time.now
+    doc.data['title'] = nil # Prevent accidental title usage
   end
 end
